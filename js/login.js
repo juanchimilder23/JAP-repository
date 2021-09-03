c1bf3bf8881dc98f793bcebbbf3eb5084f9dd519
juanchimilder23 <jamr160203@gmail.com>
2021-09-03 13:50:42 -0300

    e__2

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('boton').addEventListener("click", function () {
+        let primerCampo = document.getElementById("email");
+        let segundoCampo = document.getElementById("contraseña");
+        let camposComp = true;
+
+        if (primerCampo.value === "") {
+            primerCampo.classList.add("Invalido");
+            camposComp = false;
+        } else {
+            primerCampo.classList.remove("Invalido");
+        }
+
+        if (segundoCampo.value === '') {
+            segundoCampo.classList.add("Invalido");
+            camposComp = false;
+        } else {
+            segundoCampo.classList.remove("Invalido")
+        }
+
+
+    })
 
 });
--- js/products.js
+++ js/products.js
@@ -2,5 +2,40 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    cargarDatos(PRODUCTS_URL)
 });
+
+function cargarDatos(url) {
+    document.getElementById("products").innerHTML = "";
+    fetch(url)
+        .then(respuesta => respuesta.json())
+
+        .then(datos => {
+            datos.forEach(element => {
+
+
+                let row = "";
+                row = `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + element.imgSrc + `" alt="` + element.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ element.name + `</h4>                          
+                            <small class="text-muted">` + element.soldCount + ` artículos `
+                    + element.cost + ` ` + element.currency + ` c/u </small>
+                                             
+                        </div>
+                        <p class="mb-1">` + element.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+
+                document.getElementById("products").innerHTML += row;
+            });
+        })
+        .catch(error => alert("Hubo un error: " + error));
+}
